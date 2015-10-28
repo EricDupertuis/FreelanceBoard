6872a17067ac9f6eec5c32a3d7e07b43cd51ee95
EricDupertuis <dupertuis.eric@gmail.com>
2015-10-28 21:22:03 +0100

    Client - contact class

---
+++ src/WebRobot/FreelanceBundle/Entity/Client.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WebRobot\FreelanceBundle\Entity;
+
+/**
+ * Class Client
+ *
+ * @package WebRobot\FreelanceBundle\Entity
+ *
+ * @author Eric Dupertuis <dupertuis.eric@gmail.com>
+ */
+class Client
+{
+    protected $id;
+
+    protected $name;
+
+    protected $address;
+
+    protected $phone;
+
+    protected $email;
+
+    protected $contacts;
+
+    protected $invoices;
+}
---
+++ src/WebRobot/FreelanceBundle/Entity/Contact.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace WebRobot\FreelanceBundle\Entity;
+
+
+class Contact
+{
+    protected $id;
+
+    protected $fullName;
+
+    protected $phone;
+
+    protected $email;
+
+    protected $language;
+
+    protected $notes;
+}
