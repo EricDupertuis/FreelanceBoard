bb2300b7f3a7e7cfeb03affcc88df2c082ab8ebb
EricDupertuis <dupertuis.eric@gmail.com>
2015-10-29 21:04:45 +0100

    User class

---
+++ src/WebRobot/FreelanceBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eric
+ * Date: 10/29/15
+ * Time: 8:35 PM
+ */
+
+namespace WebRobot\FreelanceBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
