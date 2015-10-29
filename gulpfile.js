81a978628338bc9ffd087a1d918734dc5cea2ae6
EricDupertuis <dupertuis.eric@gmail.com>
2015-10-29 22:23:51 +0100

    Minify, plumber, filesize gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var filesize = require('gulp-filesize');
+var minifyCss = require('gulp-minify-css');
+var plumber = require('gulp-plumber');
 
 gulp.task('sass', function () {
     gulp.src('./src/WebRobot/FreelanceBundle/Resources/Assets/sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(filesize())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./src/WebRobot/FreelanceBundle/Resources/public/css'));
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./src/WebRobot/FreelanceBundle/Resources/public/css'))
+        .pipe(filesize());
 });
 
 gulp.task('sass:watch', function () {
