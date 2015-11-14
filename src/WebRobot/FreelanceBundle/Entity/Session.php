9e0e3eccb10c7e2c2753e3fcbfaa0665378d4c35
EricDupertuis <dupertuis.eric@gmail.com>
2015-11-14 20:11:10 +0100

    More repo

--- src/WebRobot/FreelanceBundle/Entity/Session.php
+++ src/WebRobot/FreelanceBundle/Entity/Session.php
@@ -13,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Entity
  * @ORM\Table(name="sessions")
+ * @ORM\Entity(repositoryClass="WebRobot\FreelanceBundle\Repository\SessionRepository")
  */
 class Session
 {
---
+++ src/WebRobot/FreelanceBundle/Repository/SessionRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WebRobot\FreelanceBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class SessionRepository extends EntityRepository
+{
+
+}
