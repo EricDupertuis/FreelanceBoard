d77d5048ebc78d62d51b8e6c3d4c7479fa668183
Eric Dupertuis <dupertuis.eric@gmail.com>
2015-12-09 09:41:22 +0100

    remove method

--- src/WebRobot/FreelanceBundle/Controller/DefaultController.php
+++ src/WebRobot/FreelanceBundle/Controller/DefaultController.php
@@ -3,16 +3,11 @@
 namespace WebRobot\FreelanceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use WebRobot\FreelanceBundle\Entity\Session;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $unpaidSessions = $this->getDoctrine()
-            ->getRepository('WebRobotFreelanceBundle:Session')
-            ->getAllUnpaid();
-
-        return $this->render('WebRobotFreelanceBundle::index.html.twig', ['unpaid' => $unpaidSessions]);
+        return $this->render('WebRobotFreelanceBundle::index.html.twig');
     }
 }
