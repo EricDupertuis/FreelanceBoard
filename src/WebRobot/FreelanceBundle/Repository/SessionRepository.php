a1fc7a414c5845162c30064770e30de94e4c75db
Eric Dupertuis <dupertuis.eric@gmail.com>
2015-12-09 09:41:33 +0100

    views and routing re init

--- src/WebRobot/FreelanceBundle/Repository/SessionRepository.php
+++ src/WebRobot/FreelanceBundle/Repository/SessionRepository.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\EntityRepository;
 
 class SessionRepository extends EntityRepository
 {
-
     public function getAllUnpaid()
     {
         $unpaidSessions = $this->getEntityManager()
@@ -16,4 +15,12 @@ class SessionRepository extends EntityRepository
         return $unpaidSessions;
     }
 
+    public function getAllUnpaidByProject($projectId)
+    {
+        $unpaidSessions = $this->getEntityManager()
+            ->getRepository('WebRobotFreelanceBundle:Session')
+            ->findBy(['paid' => 0, 'project' => $projectId]);
+
+        return $unpaidSessions;
+    }
 }
