d9fd6d26829239f95629e9627eed7f8df9a00585
Eric Dupertuis <dupertuis.eric@gmail.com>
2015-12-20 20:01:46 +0100

    rest

--- src/WebRobot/FreelanceBundle/Controller/ProjectsController.php
+++ src/WebRobot/FreelanceBundle/Controller/ProjectsController.php
@@ -6,10 +6,22 @@ use FOS\RestBundle\Controller\FOSRestController;
 
 class ProjectsController extends FOSRestController
 {
+    public function getAllAction()
+    {
+        $projects = $this->container->get('doctrine.orm.entity_manager')
+            ->getRepository('WebRobotFreelanceBundle:Project')
+            ->findAll();
+
+        $view = $this->view($projects, 200);
+
+        return $this->handleView($view);
+    }
+
     public function getProject($id)
     {
         return $this->container->get('doctrine.orm.entity_manager')
             ->getRepository('WebRobotFreelanceBundle:Project')
             ->find($id);
+
     }
 }
