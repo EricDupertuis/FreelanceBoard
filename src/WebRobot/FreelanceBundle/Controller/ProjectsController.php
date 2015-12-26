f85df90b7a68cf342d183fdd62abc98604e2674a
Eric Dupertuis <dupertuis.eric@gmail.com>
2015-12-26 20:41:19 +0100

    rework separation

--- src/WebRobot/FreelanceBundle/Controller/ProjectsController.php
+++ src/WebRobot/FreelanceBundle/Controller/ProjectsController.php
@@ -3,9 +3,14 @@
 namespace WebRobot\FreelanceBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ProjectsController extends FOSRestController
 {
+
+    /**
+     * @Route("/all", name="get_all_projects")
+     */
     public function getAllAction()
     {
         $projects = $this->container->get('doctrine.orm.entity_manager')
