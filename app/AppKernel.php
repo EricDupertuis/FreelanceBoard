38caf19e475f2e18506a6675e6f0db845976b53f
Eric Dupertuis <dupertuis.eric@gmail.com>
2015-12-26 20:42:57 +0100

    add ApiBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new \JMS\SerializerBundle\JMSSerializerBundle(),
             new \Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new WebRobot\FreelanceBundle\WebRobotFreelanceBundle(),
+            new WebRobot\ApiBundle\WebRobotApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/WebRobot/ApiBundle/WebRobotApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WebRobot\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebRobotApiBundle extends Bundle
+{
+}
