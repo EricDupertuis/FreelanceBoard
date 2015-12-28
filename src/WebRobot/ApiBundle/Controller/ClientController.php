8bba93f3b6525a73897db18812289f43d407f7c1
Eric Dupertuis <dupertuis.eric@gmail.com>
2015-12-28 18:35:37 +0100

    bullshit rest

--- src/WebRobot/ApiBundle/Controller/ClientController.php
+++ src/WebRobot/ApiBundle/Controller/ClientController.php
@@ -2,23 +2,9 @@
 
 namespace WebRobot\ApiBundle\Controller;
 
-use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ClientController extends Controller
 {
-    public function getAllAction()
-    {
-        $clients = $this->getDoctrine()->getRepository('WebRobotFreelanceBundle:Client')
-            ->findAll();
-
-        if (!is_object($clients)) {
-            return false;
-        }
-
-        return $clients;
-
-    }
 
 }
--- src/WebRobot/ApiBundle/Controller/ProjectController.php
+++ src/WebRobot/ApiBundle/Controller/ProjectController.php
@@ -2,35 +2,9 @@
 
 namespace WebRobot\ApiBundle\Controller;
 
-use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ProjectController extends Controller
 {
-    public function getAllAction()
-    {
-        $project = $this->getDoctrine()->getRepository('WebRobotFreelanceBundle:Project')->findAll();
-        if (!is_object($project)) {
-            return false;
-        }
-        return $project;
-    }
 
-    public function getAction($id)
-    {
-        $project = $this->getDoctrine()->getRepository('WebRobotFreelanceBundle:Project')
-            ->findOneBy(['id' => $id]);
-
-        if (!is_object($project) || $project == false) {
-            return false;
-        }
-
-        return $project;
-    }
-
-    public function createAction()
-    {
-        return false;
-    }
 }
