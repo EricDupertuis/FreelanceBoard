1c762e605a8add11edf22d78e5e9b6ce565aa85c
Eric Dupertuis <dupertuis.eric@gmail.com>
2015-12-29 23:48:14 +0100

    get projects and route path

--- src/WebRobot/ApiBundle/Controller/ProjectController.php
+++ src/WebRobot/ApiBundle/Controller/ProjectController.php
@@ -3,8 +3,23 @@
 namespace WebRobot\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use WebRobot\FreelanceBundle\Entity\Project;
 
 class ProjectController extends Controller
 {
+    public function getProjectsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
 
+        $projects = $em->getRepository('WebRobotFreelanceBundle:Project')
+            ->findBy(['user' => $this->getUser()]);
+
+        if (!$projects) {
+            throw $this->createNotFoundException('No projects found');
+        }
+
+        return $projects;
+    }
 }
