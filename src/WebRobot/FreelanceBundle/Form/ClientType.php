dfa31dfab1ba3c7b7f9ee343354f127d3c4b2972
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-02-29 18:20:33 +0100

    crud autogeneration for clients

---
+++ src/WebRobot/FreelanceBundle/Form/ClientType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WebRobot\FreelanceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ClientType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WebRobot\FreelanceBundle\Entity\Client'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'webrobot_freelancebundle_client';
+    }
+}
