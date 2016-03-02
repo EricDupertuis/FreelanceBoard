a02a4c21e204f958a5c2d258151d42901de13ca4
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-03-02 14:23:39 +0100

    automatic crud generation for /projects

---
+++ src/WebRobot/FreelanceBundle/Controller/ProjectController.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace WebRobot\FreelanceBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use WebRobot\FreelanceBundle\Entity\Project;
+use WebRobot\FreelanceBundle\Form\ProjectType;
+
+/**
+ * Project controller.
+ *
+ */
+class ProjectController extends Controller
+{
+
+    /**
+     * Lists all Project entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('WebRobotFreelanceBundle:Project')->findAll();
+
+        return $this->render('WebRobotFreelanceBundle:Project:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Project entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Project();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('projects_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('WebRobotFreelanceBundle:Project:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Project entity.
+     *
+     * @param Project $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Project $entity)
+    {
+        $form = $this->createForm(new ProjectType(), $entity, array(
+            'action' => $this->generateUrl('projects_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Project entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Project();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('WebRobotFreelanceBundle:Project:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Project entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('WebRobotFreelanceBundle:Project')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('WebRobotFreelanceBundle:Project:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Project entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('WebRobotFreelanceBundle:Project')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('WebRobotFreelanceBundle:Project:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Project entity.
+    *
+    * @param Project $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Project $entity)
+    {
+        $form = $this->createForm(new ProjectType(), $entity, array(
+            'action' => $this->generateUrl('projects_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Project entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('WebRobotFreelanceBundle:Project')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('projects_edit', array('id' => $id)));
+        }
+
+        return $this->render('WebRobotFreelanceBundle:Project:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Project entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('WebRobotFreelanceBundle:Project')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Project entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('projects'));
+    }
+
+    /**
+     * Creates a form to delete a Project entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('projects_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
