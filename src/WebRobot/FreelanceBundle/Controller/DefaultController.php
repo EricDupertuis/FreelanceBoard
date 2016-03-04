4c244f97e111f22e4a2ecdcea1f5655166428ffc
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-03-04 23:49:03 +0100

    get clients in homepage controller

--- src/WebRobot/FreelanceBundle/Controller/DefaultController.php
+++ src/WebRobot/FreelanceBundle/Controller/DefaultController.php
@@ -10,15 +10,21 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
+
+        $user = $this->getUser();
+
         $projects = $em->getRepository('WebRobotFreelanceBundle:Project')
-            ->findBy(['user' => $this->getUser()]);
+            ->findBy(['user' => $user]);
 
         $total = $em->getRepository('WebRobotFreelanceBundle:Session')
-            ->getUnpaidTotal($this->getUser());
+            ->getUnpaidTotal($user);
+
+        $clients = $em->getRepository('WebRobotFreelanceBundle:Client')
+            ->findBy(['user' => $user]);
 
         return $this->render(
             'WebRobotFreelanceBundle::index.html.twig',
-            ['projects' => $projects, 'total' => $total]
+            ['projects' => $projects, 'total' => $total, 'clients' => $clients]
         );
     }
 }
