304ff27371c043631c3aa25c17847a23177fe8f3
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-07-25 21:24:15 +0200

    wip

--- src/WebRobot/FreelanceBundle/Form/ContactType.php
+++ src/WebRobot/FreelanceBundle/Form/ContactType.php
@@ -3,8 +3,11 @@
 namespace WebRobot\FreelanceBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WebRobot\FreelanceBundle\Entity\Client;
 
 class ContactType extends AbstractType
 {
@@ -19,15 +22,20 @@ class ContactType extends AbstractType
             ->add('phone')
             ->add('email')
             ->add('language')
-            ->add('notes')
-            ->add('client')
+            ->add('notes', TextareaType::class, [
+
+            ])
+            ->add('client', 'genemu_jqueryselect2_entity', [
+                'class' => 'WebRobot\FreelanceBundle\Entity\Client',
+                'property' => 'name',
+            ])
         ;
     }
     
     /**
-     * @param OptionsResolverInterface $resolver
+     * @param OptionsResolver $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'WebRobot\FreelanceBundle\Entity\Contact'
